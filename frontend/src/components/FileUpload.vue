0d062866f45c85f5ed153152ff5a735d05d9d368
danielmachado <danielmachadopintos@gmail.com>
2024-01-18 14:31:07 -0300

    Remove commented out code for reading XLSX and CSV files

--- frontend/src/components/FileUpload.vue
+++ frontend/src/components/FileUpload.vue
@@ -41,35 +41,6 @@ export default {
           console.error("Erro ao enviar arquivos:", error);
         });
     },
-    // readXLSXFile(file) {
-    //   const reader = new FileReader();
-    //   reader.onload = (e) => {
-    //     const data = e.target.result;
-    //     const workbook = XLSX.read(data, { type: "binary" });
-
-    //     const firstSheetName = workbook.SheetNames[0];
-    //     const worksheet = workbook.Sheets[firstSheetName];
-
-    //     // Converte a planilha para JSON
-    //     const json = XLSX.utils.sheet_to_json(worksheet);
-
-    //     console.log("Dados da Planilha XLSX:", json);
-    //   };
-    //   reader.readAsBinaryString(file);
-    // },
-    // parseCSV(event) {
-    //   const file = event.target.files[0];
-    //   if (!file) return;
-
-    //   Papa.parse(file, {
-    //     complete: (results) => {
-    //       console.log("Dados do CSV:", results.data);
-    //     },
-    //     header: true, // Usa a primeira linha como cabeçalhos
-    //     skipEmptyLines: true,
-    //     dynamicTyping: true,
-    //   });
-    // },
   },
 };
 </script>

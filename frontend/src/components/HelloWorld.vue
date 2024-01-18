ef6de672d8e487d61e465c72cb234580eaa30a84
danielmachado <danielmachadopintos@gmail.com>
2024-01-17 23:30:03 -0300

    feat: add dependencies for file parsing
    Update package.json and HelloWorld.vue
    
    Add file parsing methods to HelloWorld.vue

--- frontend/src/components/HelloWorld.vue
+++ frontend/src/components/HelloWorld.vue
@@ -3,15 +3,63 @@
     <v-row class="text-center">
       <v-col cols="12">
         <h1 class="display-1">Envio de arquivo</h1>
+        <v-file-input
+          label="Selecione um arquivo"
+          accept=".xlsx,.csv"
+          @change="handleFileUpload"
+        ></v-file-input>
       </v-col>
     </v-row>
   </v-container>
 </template>
 
 <script>
+import XLSX from "xlsx";
+import Papa from "papaparse";
+
 export default {
   name: "HelloWorld",
 
   data: () => ({}),
+  methods: {
+    handleFileUpload(event) {
+      const file = event.target.files[0];
+      if (!file) return;
+
+      // Verifica a extensão do arquivo
+      const fileExtension = file.name.split(".").pop().toLowerCase();
+
+      if (fileExtension === "xlsx") {
+        this.readXLSXFile(file);
+      } else if (fileExtension === "csv") {
+        this.parseCSV(event);
+      } else {
+        // Mensagem de erro ou lógica para tipos de arquivo inválidos
+        console.error("Tipo de arquivo não suportado");
+      }
+    },
+    readXLSXFile(file) {
+      const reader = new FileReader();
+      reader.onload = (e) => {
+        const data = e.target.result;
+        const workbook = XLSX.read(data, { type: "binary" });
+        console.log("workbook", workbook);
+      };
+      reader.readAsBinaryString(file);
+    },
+    parseCSV(event) {
+      const file = event.target.files[0];
+      if (!file) return;
+
+      Papa.parse(file, {
+        complete: (results) => {
+          console.log("Resultados do Parse:", results.data);
+        },
+        header: true, // Define se a primeira linha do CSV contém os cabeçalhos
+        skipEmptyLines: true, // Ignora linhas vazias
+        dynamicTyping: true, // Converte automaticamente strings numéricas e literais booleanos
+      });
+    },
+  },
 };
 </script>

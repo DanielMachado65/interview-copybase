25e2bafd2a93a57c63fd653b002b8311aa0dca5c
danielmachado <danielmachadopintos@gmail.com>
2024-01-18 13:53:22 -0300

    Add axios for file upload and update file upload logic

--- frontend/src/components/FileUpload.vue
+++ frontend/src/components/FileUpload.vue
@@ -2,17 +2,17 @@
   <v-row class="text-center">
     <v-col cols="12">
       <v-file-input
-        label="Selecione um arquivo"
+        label="Selecione arquivos"
         accept=".xlsx,.csv"
         @change="handleFileUpload"
+        multiple
       ></v-file-input>
     </v-col>
   </v-row>
 </template>
 
 <script>
-import * as XLSX from "xlsx";
-import Papa from "papaparse";
+import axios from "axios";
 
 export default {
   name: "FileUpload",
@@ -20,50 +20,56 @@ export default {
   data: () => ({}),
   methods: {
     handleFileUpload(event) {
-      const file = event.target.files[0];
-      if (!file) return;
+      const files = event.target.files;
+      if (!files.length) return;
 
-      // Verifica a extensão do arquivo
-      const fileExtension = file.name.split(".").pop().toLowerCase();
-
-      if (fileExtension === "xlsx") {
-        this.readXLSXFile(file);
-      } else if (fileExtension === "csv") {
-        this.parseCSV(event);
-      } else {
-        // Mensagem de erro ou lógica para tipos de arquivo inválidos
-        console.error("Tipo de arquivo não suportado");
+      const formData = new FormData();
+      for (let i = 0; i < files.length; i++) {
+        formData.append("files", files[i]);
       }
+
+      axios
+        .post(process.env.VUE_APP_BASE_API + "/file-upload", formData, {
+          headers: {
+            "Content-Type": "multipart/form-data",
+          },
+        })
+        .then((response) => {
+          console.log("Arquivos enviados com sucesso:", response);
+        })
+        .catch((error) => {
+          console.error("Erro ao enviar arquivos:", error);
+        });
     },
-    readXLSXFile(file) {
-      const reader = new FileReader();
-      reader.onload = (e) => {
-        const data = e.target.result;
-        const workbook = XLSX.read(data, { type: "binary" });
+    // readXLSXFile(file) {
+    //   const reader = new FileReader();
+    //   reader.onload = (e) => {
+    //     const data = e.target.result;
+    //     const workbook = XLSX.read(data, { type: "binary" });
 
-        const firstSheetName = workbook.SheetNames[0];
-        const worksheet = workbook.Sheets[firstSheetName];
+    //     const firstSheetName = workbook.SheetNames[0];
+    //     const worksheet = workbook.Sheets[firstSheetName];
 
-        // Converte a planilha para JSON
-        const json = XLSX.utils.sheet_to_json(worksheet);
+    //     // Converte a planilha para JSON
+    //     const json = XLSX.utils.sheet_to_json(worksheet);
 
-        console.log("Dados da Planilha XLSX:", json);
-      };
-      reader.readAsBinaryString(file);
-    },
-    parseCSV(event) {
-      const file = event.target.files[0];
-      if (!file) return;
+    //     console.log("Dados da Planilha XLSX:", json);
+    //   };
+    //   reader.readAsBinaryString(file);
+    // },
+    // parseCSV(event) {
+    //   const file = event.target.files[0];
+    //   if (!file) return;
 
-      Papa.parse(file, {
-        complete: (results) => {
-          console.log("Dados do CSV:", results.data);
-        },
-        header: true, // Usa a primeira linha como cabeçalhos
-        skipEmptyLines: true,
-        dynamicTyping: true,
-      });
-    },
+    //   Papa.parse(file, {
+    //     complete: (results) => {
+    //       console.log("Dados do CSV:", results.data);
+    //     },
+    //     header: true, // Usa a primeira linha como cabeçalhos
+    //     skipEmptyLines: true,
+    //     dynamicTyping: true,
+    //   });
+    // },
   },
 };
 </script>

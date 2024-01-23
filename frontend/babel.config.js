29de55e47a863b8914a89d3df8d079240b7b4ab9
Daniel Machado <danielmachadopintos@gmail.com>
2024-01-23 10:55:28 -0300

    Add transform-define plugin to Babel config and package.json

--- frontend/babel.config.js
+++ frontend/babel.config.js
@@ -1,3 +1,13 @@
 module.exports = {
   presets: ["@vue/cli-plugin-babel/preset"],
+  plugins: [
+    [
+      "transform-define",
+      {
+        __VUE_OPTIONS_API__: true,
+        __VUE_PROD_DEVTOOLS__: false,
+        __VUE_PROD_HYDRATION_MISMATCH_DETAILS__: false,
+      },
+    ],
+  ],
 };

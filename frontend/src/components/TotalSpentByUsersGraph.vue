d431a8fc0875c17b57bccab4e7ee343813e27658
Daniel Machado <danielmachadopintos@gmail.com>
2024-01-23 09:49:59 -0300

    Refactor DashboardGraph component to use separate graph components

---
+++ frontend/src/components/TotalSpentByUsersGraph.vue
@@ -0,0 +1,62 @@
+<template>
+  <v-col cols="12" md="6">
+    <h2>Total Spent by Users</h2>
+    <LineChart :chart-data="usersValueChartData" :options="chartOptions" />
+  </v-col>
+</template>
+
+<script>
+import { defineComponent } from "vue";
+import { LineChart } from "vue-chart-3";
+import axios from "axios";
+
+export default defineComponent({
+  name: "TotalSpentByUsersGraph",
+  props: {
+    fileId: {
+      type: String,
+      required: true,
+    },
+  },
+  components: {
+    LineChart,
+  },
+  data: () => ({
+    usersValueChartData: {
+      labels: [],
+      datasets: [
+        {
+          label: "Total Spent",
+          backgroundColor: "#79a6f8",
+          borderColor: "#79a6f8",
+          data: [],
+        },
+      ],
+    },
+  }),
+  methods: {
+    async fetchMRRData() {
+      axios
+        .get(
+          process.env.VUE_APP_BASE_API +
+            "/metrics/" +
+            this.fileId +
+            "/group-by-users-per-value"
+        )
+        .then((response) => {
+          this.processUsersValueData(response.data);
+        });
+    },
+    processUsersValueData(usersValueData) {
+      const labels = usersValueData.map((item) => item.subscriberId);
+      const data = usersValueData.map((item) => item.totalSpent);
+
+      this.usersValueChartData.labels = labels;
+      this.usersValueChartData.datasets[0].data = data;
+    },
+  },
+  mounted() {
+    this.fetchMRRData();
+  },
+});
+</script>
---
+++ frontend/src/components/index.js
@@ -0,0 +1,11 @@
+import TotalSpentByUsersGraph from "./TotalSpentByUsersGraph";
+import StatusDistributionGraph from "./StatusDistributionGraph";
+import MRRGraph from "./MRRGraph";
+import ChurnGraph from "./ChurnGraph";
+
+export {
+  TotalSpentByUsersGraph,
+  StatusDistributionGraph,
+  MRRGraph,
+  ChurnGraph,
+};

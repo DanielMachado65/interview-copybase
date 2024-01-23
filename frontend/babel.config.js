bf7b28a39507d1580aa6001bd91550d9333ba148
Daniel Machado <danielmachadopintos@gmail.com>
2024-01-23 10:56:05 -0300

    Update Vue production settings

--- frontend/babel.config.js
+++ frontend/babel.config.js
@@ -5,8 +5,8 @@ module.exports = {
       "transform-define",
       {
         __VUE_OPTIONS_API__: true,
-        __VUE_PROD_DEVTOOLS__: false,
-        __VUE_PROD_HYDRATION_MISMATCH_DETAILS__: false,
+        __VUE_PROD_DEVTOOLS__: true,
+        __VUE_PROD_HYDRATION_MISMATCH_DETAILS__: true,
       },
     ],
   ],
